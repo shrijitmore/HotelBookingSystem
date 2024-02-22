872f9910470affd8cb14cf4dc8b3c5aacf7d684e
Mahesh <maheshkadam.se@gmail.com>
2024-02-22 23:27:33 +0530

    testing header2

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,6 +8,7 @@ export default function Navbar({ fixed }) {
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <a
+            
               className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
               href="#pablo"
             >
@@ -34,7 +35,7 @@ export default function Navbar({ fixed }) {
                   className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
                   href="#pablo"
                 >
-                  <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Share</span>
+                  <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Share Me</span>
                 </a>
               </li>
               <li className="nav-item">
