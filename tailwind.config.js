70cc5d750f9394d37b1fde7aa20319e75578cf1f
Mahesh <maheshkadam.se@gmail.com>
2024-02-22 23:07:50 +0530

    first commit header

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    extend: { 
+      colors:
+      {
+    "dark-blue":"#121c28"
+      }
+    },
+  },
+  plugins: [],
+}
+
